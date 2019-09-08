0cbc414c06a0ec76746929eb12d2e255d294b62d
714400809 <42685868+714400809@users.noreply.github.com>
2019-09-08 16:01:29 +0800

    Update Region.java

--- Region.java
+++ Region.java
@@ -5,6 +5,6 @@ import java.util.ArrayList;
 public class Region {
 	public String name = new String();//区域名
 	public int value = 0;//区域均价
-	public int id = 0;
+	public int id = 1;
 	public ArrayList<Building> type = new ArrayList<Building>();
 }
