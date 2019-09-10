1fc5c2b5ed4cac0454ed52bc67942df56cf19d0e
714400809 <42685868+714400809@users.noreply.github.com>
2019-09-10 10:35:56 +0800

    Update Building.java

--- Building.java
+++ Building.java
@@ -3,6 +3,8 @@ package com.db;
 import java.util.Date;
 import java.util.ArrayList;
 
+//此类为楼盘的封装类，包含了楼盘的各种信息，测试成功
+//创建人：李名啟		创建时间：20190825	最近更新时间：20190903
 public class Building {
 	public String name = new String();//楼盘名
 	public int value = 0;//楼盘均价
