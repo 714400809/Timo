92df9dc9880616403178345e354ab10f0f45fb40
714400809 <42685868+714400809@users.noreply.github.com>
2019-09-10 10:39:24 +0800

    Update Region.java

--- Region.java
+++ Region.java
@@ -2,6 +2,8 @@ package com.db;
 
 import java.util.ArrayList;
 
+//此类为区域的封装类，包含了区域的各种信息，测试成功
+//创建人：李名啟		创建时间：20190825	最近更新时间：20190825
 public class Region {
 	public String name = new String();//区域名
 	public int value = 0;//区域均价
