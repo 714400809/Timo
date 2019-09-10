a5e5c2dd0d31a826011323a03f3611e8ec1b4518
714400809 <42685868+714400809@users.noreply.github.com>
2019-09-10 10:36:20 +0800

    Update BuildingType.java

--- BuildingType.java
+++ BuildingType.java
@@ -1,5 +1,7 @@
-﻿package com.db;
+package com.db;
 
+//此类为楼盘具体户型的封装类，包含了楼盘具体户型的各种信息，测试成功
+//创建人：李名啟		创建时间：20190825	最近更新时间：20190828
 public class BuildingType {
 	public String name = new String();//户型名
 	public int value = 0;//户型价格
