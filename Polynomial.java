c85d407e7aaf3ccc7bce22da004cba5e84b80b78
714400809 <42685868+714400809@users.noreply.github.com>
2019-09-10 10:39:03 +0800

    Update Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,6 +1,7 @@
 package com.db;
 
-//测试成功
+//此类为多项式类，用于进行多项式的存储和计算，测试成功
+//创建人：龚灿	创建时间：20190828	最近更新时间：20190828
 public class Polynomial {
 	double[] coefficients;//系数数组
 	int power;//最高次
@@ -13,8 +14,9 @@ public class Polynomial {
 		}
 	}
 	
-	//该函数用于计算多项式特定x值下的y值，20190828龚灿，测试成功
-	//输入为自变量x
+	//该函数用于计算多项式特定x值下的y值，测试成功
+	//创建人：龚灿	创建时间：20190828	最近更新时间：20190828
+	//输入为自变量x，输出为对应得因变量y
 	double getValue(double x) {
 		double y = coefficients[power];
 		for(int i=power-1;i>=0;--i) {
