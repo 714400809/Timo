465194983e084431a3bb7cbfe5fd01d7cfc52686
714400809 <42685868+714400809@users.noreply.github.com>
2019-09-10 10:37:40 +0800

    Update DateOperation.java

--- DateOperation.java
+++ DateOperation.java
@@ -4,10 +4,12 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-//测试成功
+//此类用于进行String与Date间的转换，并包含有获取以前的时间信息的功能，测试成功
+//创建人：龚灿	创建时间：20190827	最近更新时间：20190827
 public class DateOperation {
-	//该函数用于获取上past个月的第一天日期，以字符串形式返回，20190827龚灿，测试成功
-	//输入为上几个月，past=0得到当月第一天日期
+	//该函数用于获取上past个月的第一天日期，以字符串形式返回，测试成功
+	//创建人：龚灿	创建时间：20190827	最近更新时间：20190827
+	//输入为上几个月，输出为past个月前的第一天日期，past=0得到当月第一天日期
 	static String getPastFirst(int past) {
 		if(past<0 || past>11)
 			return null;
@@ -23,6 +25,7 @@ public class DateOperation {
 		}
 		str[2] = "01";
 		String pastDateString = new String();
+		//统一格式yyyy-MM-dd
 		if(month<10)
 			pastDateString = String.valueOf(year)+"-0"+String.valueOf(month)+"-"+str[2];
 		else
@@ -30,7 +33,9 @@ public class DateOperation {
 		return pastDateString;
 	}
 	
-	//该函数用于获取下个月第一天的日期，以字符串形式返回，20190827龚灿，测试成功
+	//该函数用于获取下个月第一天的日期，以字符串形式返回，测试成功
+	//创建人：龚灿	创建时间：20190827	最近更新时间：20190827
+	//无需输入，输出为下个月第一天的日期
 	static String getNextFirst() {
 		Date currentTime = new Date();
 		String currentDateString = getStringDate(currentTime);
@@ -44,6 +49,7 @@ public class DateOperation {
 		}
 		str[2] = "01";
 		String pastDateString = new String();
+		//统一格式yyyy-MM-dd
 		if(month<10)
 			pastDateString = String.valueOf(year)+"-0"+String.valueOf(month)+"-"+str[2];
 		else
@@ -51,14 +57,16 @@ public class DateOperation {
 		return pastDateString;
 	}
 	
-	//Date转String，20190827龚灿，测试成功
+	//Date转String，测试成功
+	//创建人：龚灿	创建时间：20190827	最近更新时间：20190827
 	public static String getStringDate(Date time) {
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 		String dateStr = formatter.format(time);
 		return dateStr;
 	}
 	
-	//String转Date，20190827龚灿，测试成功
+	//String转Date，测试成功
+	//创建人：龚灿	创建时间：20190827	最近更新时间：20190827
 	public static Date getDate(String str) throws ParseException {
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
     	Date date = formatter.parse(str);
