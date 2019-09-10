755027ad7e6c89d9f0e06da7d0f7057fedc8ccdf
714400809 <42685868+714400809@users.noreply.github.com>
2019-09-10 10:35:24 +0800

    Update Abbreviation.java

--- Abbreviation.java
+++ Abbreviation.java
@@ -6,8 +6,12 @@ import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
 import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
 import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 
+//此类专用于生成静态页面时根据楼盘名缩写生成对应网址，测试成功
+//创建人：龚灿	创建时间：20190821	最近更新时间：20190910
 public class Abbreviation {
-	//此静态函数用于获取中文的首拼音缩写，20190821龚灿，测试成功
+	//此静态函数用于获取中文的首拼音缩写，会保留可能出现在楼盘名中的其他特殊字符，测试成功
+	//创建人：龚灿	创建时间：20190821	最近更新时间：20190910
+	//输入为待处理字符串，输出为字符串小写缩写
 	public static String getAbbreviation(String chinese) throws BadHanyuPinyinOutputFormatCombination {
         char[] charArray = chinese.toCharArray();
         StringBuilder pinyin = new StringBuilder();
@@ -24,8 +28,8 @@ public class Abbreviation {
                     pinyin.append(hanyuPinyinStringArray[0].charAt(0));
                 }
             }
-            //匹配数字和字母，将其在对应位置保留
-            if (Character.toString(charArray[i]).matches("[A-Za-z0-9]+")) {
+            //匹配数字、字母和一些特殊字符，将其在对应位置保留
+            if (Character.toString(charArray[i]).matches("[°·)(A-Za-z0-9]+")) {
                 pinyin.append(charArray[i]);
             }
         }
